bbb84259b7cd2076ec401928305bb447250a1378
Nicole Perisic <nico.perisic@gmail.com>
2021-05-20 21:46:52 -0400

    Arreglo de templates y css

--- src/templates/templatemuro.js
+++ src/templates/templatemuro.js
@@ -1,7 +1,17 @@
+import { footer } from '../componentes/footer.js';
+
 export const muro = () => {
   const viewMuro = document.createElement('div');
   viewMuro.id = 'muro';
   viewMuro.appendChild(nav());
+  
 
-  return viewMuro;
+
+
+  const divMuro = document.createElement('div');
+  const divMuroContainer = document.createElement('div');
+  divMuro.innerHTML = viewMuro;
+  divMuroContainer.appendChild(footer());
+
+  return viewMuroContainer;
 };
--- src/templates/templateregister.js
+++ src/templates/templateregister.js
@@ -5,21 +5,16 @@ export const register = () => {
   <div id="contenedorLogin">
   <img class="portada">
   <div id="divContenidoLogin">
-    <form id="formLogin">
-      <label for="email">Correo:</label><br><input placeholder="ingresa tu correo" type="text" id="emailHome"
-        class="inputHome" name="email"><br>
-      <label for="password">Contraseña:</label><br><input placeholder="ingresa tu contraseña" type="password"
-        id="passwordHome" class="inputHome" name="password"><br>
-      <button class="btnLogin">Login</button>
-    </form>
-    <div id="elementosGoogle">
-      <div id="textoUno">¿No tienes cuenta? Registrate <a href="/#/register"><span id="aqui">aquí</span></a></div>
-      <div id="textoDos"> O ingresa con tu cuerta de Google</div>
-      <a class="fab fa-google fa-3x" id="google" onclick="authGoogle()"></a>
-      <div id="textoTres">¿Olvidaste tu contraseña?, recuperala <a><span id="aquiTwo">aquí</span></a></div>
-    </div>
+  <form id="formRegistro"> 
+  <label for="username">Nombre:</label><br><input placeholder="ingresa tu nombre" type="text" id="username" class="inputRegistro" name="username"><br> 
+  <label for="email">Email:</label><br><input placeholder="Ingresa un correo valido" type="text" id="email" class="inputRegistro" name="email"><br> 
+  <label for="password">Contraseña:</label><br><input placeholder="Ingresa una contraseña" type="password" id="password" class="inputRegistro" name="password"><br> 
+  <label for="confirmpassword"></label><br><input placeholder="Confirma tu contraseña" type="password" id="confpassword" class="inputRegistro" name="confirmpassword"><br> 
+  <button type="submit" id="btnConfirmar">CONFIRMAR</button> 
+  <button type="submit" id="btnCancelar">CANCELAR</button> 
+  </form>
   </div>
-</div>`;
+  </div>`;
 
   const divLogin = document.createElement('div');
   const divLoginContainer = document.createElement('div');
